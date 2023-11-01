0a6d95f4667ffc0ebc9f085db2afbab8e7ffb619
AlexStorm <info@alexstorm.ru>
2023-11-01 16:59:46 +0530

    configmap changed

--- src/builders/build-microfrontend.ts
+++ src/builders/build-microfrontend.ts
@@ -3,7 +3,7 @@ import {BuildController} from "./microfrontend/build-controller";
 import {extractParamsFromNodeModule, extractSharedFromPackageJson} from "./microfrontend/extractors";
 import fs from "fs";
 
-function sharedInfosToImportMapJson(sharedInfos: SharedInfo[]): Object {
+function sharedInfosToImportMapJson(sharedInfos: SharedInfo[]):{imports:{[name:string]:string}} {
     const imports:{[name:string]:string} = {}
     for (const sharedInfo of sharedInfos) {
         imports[sharedInfo.packageName] ="/shared/"+ sharedInfo.outFileName
@@ -16,7 +16,7 @@ function sharedInfosToImportMapJson(sharedInfos: SharedInfo[]): Object {
 export class MicroFrontendBuilder implements BuilderInterface {
 
     private modulePath: string;
-    constructor( moduleName: string) {
+    constructor(private moduleName: string) {
         this.modulePath = moduleName.replace("@", "")
     }
 
@@ -51,11 +51,15 @@ export class MicroFrontendBuilder implements BuilderInterface {
 
 
         console.log("Results", results)
-        console.log("Shared Infos", sharedInfos)
+
 
         const importMapPath = "./dist/modules/"+this.modulePath+"/importmap.json"
 
-        let importMapObj = sharedInfosToImportMapJson(sharedInfos);
+        const  importMapObj:{imports:{[name:string]:string}} = sharedInfosToImportMapJson(sharedInfos);
+
+        importMapObj.imports[this.moduleName]="/modules/"+this.modulePath+"/index.js"
+
+        console.log("Result importMap", importMapObj)
 
         fs.writeFileSync(importMapPath, JSON.stringify(importMapObj, null, 2));
 
