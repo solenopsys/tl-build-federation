18005a9abd1e70c9b097744aa8b356ac250b745c
AlexStorm <info@alexstorm.ru>
2023-11-01 22:25:25 +0530

    submodules

--- src/builders/microfrontend/extractors.ts
+++ src/builders/microfrontend/extractors.ts
@@ -1,29 +1,46 @@
 import fs from "fs";
 import path from "path";
 
-export function extractFromPackageJson(fileName: string): { version: string, module: string } {
+export function extractFromPackageJson(fileName: string): { version: string, module: string, exports?: { [name: string]: string } } {
     const packageJson = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-    return {version: packageJson.version, module: packageJson.module};
+    return {version: packageJson.version, module: packageJson.module, exports: packageJson.exports};
 }
 
 export function extractParamsFromNodeModule(packageName: string): { version: string, file: string } {
     let nodeModule = "./node_modules/";
     let packageJson = "/package.json";
-    const nodeDir = nodeModule + packageName
+    let splitStrings = packageName.split("/");
+    const subPackage = splitStrings.length === 3
+
+    const nodeDir =  nodeModule + (subPackage?splitStrings[0]+"/"+splitStrings[1]:packageName)
     let pathToPackageJson = nodeDir + packageJson;
     if (fs.existsSync(pathToPackageJson)) {
         let extract = extractFromPackageJson(pathToPackageJson);
 
+
         if (!extract.version) {
-            let parentPackageName = packageName.split("/")[0];
+            let parentPackageName = splitStrings[0];
             const nodeDir = nodeModule + parentPackageName
             let pathToPackageJsonParent = nodeDir + packageJson;
             let extractTop = extractFromPackageJson(pathToPackageJsonParent);
             extract.version = extractTop.version;
         }
+        let file= path.join(nodeDir, extract.module) ;
+        if (subPackage){
+            if( extract.exports){
+                let subDirectory = splitStrings[2];
+                const fn = extract.exports["./"+subDirectory]["default"]
+                file=path.join(nodeDir, fn) ;
+            }else{
+                let subDir = nodeModule+packageName;
+                let extracts = extractFromPackageJson(subDir+packageJson);
+                file=path.join(subDir, extracts.module) ;
+            }
+
+        }
         return {
             version: extract.version,
-            file: path.join(nodeDir, extract.module)
+            file: file
         }
     } else {
         throw new Error("Package not found: " + pathToPackageJson)
